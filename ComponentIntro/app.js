e420f8adfbac46a5b2a56c14338f64056adb01c0
Joonwoo Jang (Percy) <percyjang.dev@outlook.com>
2024-09-19 10:00:13 +0900

    feat: section 3, 6 complete

---
+++ ComponentIntro/app.js
@@ -0,0 +1,41 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      detailsAreVisible: false,
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "01234 5678 991",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "09876 543 221",
+          email: "julie@localhost.com",
+        },
+      ],
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+});
+
+app.mount("#app");
+
+app.component("user-card", {
+  template: `
+    <li>
+          <h2>{{friend.name}}</h2>
+          <button @click="toggleDetails">Show Details</button>
+          <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong> {{friend.phone}}</li>
+            <li><strong>Email:</strong> {{friend.email}}</li>
+          </ul>
+        </li>
+    `,
+});
--- ConditionalRendering/app.js
+++ ConditionalRendering/app.js
@@ -1,7 +1,17 @@
 const app = Vue.createApp({
   data() {
-    return { goals: [] };
+    return { goals: [], enteredGoalValue: "" };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredGoalValue);
+      console.log(this.goals);
+    },
+    removeGoal(i) {
+      this.goals.splice(i, 1);
+      console.log(this.goals);
+    }
   },
 });
 
-app.mount('#user-goals');
+app.mount("#user-goals");
